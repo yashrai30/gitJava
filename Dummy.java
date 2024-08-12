0837f938a821a5a029e1e8cb0cdf838dde89685a
Yash Rai <yash.rai@nagarro.com>
2024-08-12 14:30:06 +0530

    first commit

---
+++ Dummy.java
@@ -0,0 +1,18 @@
+public class Dummy {
+
+    public int add(int a, int b) {
+        System.out.println("Adding " + a + " and " + b);
+        return a + b;
+    }
+    
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Dummy dummy = new Dummy();
+        System.out.println("Addition: " + dummy.add(5, 3));
+        System.out.println("Subtraction: " + dummy.subtract(5, 3));
+    }
+}
